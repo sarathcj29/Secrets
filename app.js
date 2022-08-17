9eb20ffa908eaaf84ea32dc6846396b6b544049a
sarathcj29 <sarathcj29@gmail.com>
2022-08-17 08:28:33 +0530

    First commit

---
+++ app.js
@@ -0,0 +1,71 @@
+//jshint esversion:6
+const express = require('express');
+const bodyParser = require('body-parser');
+const ejs = require('ejs');
+const mongoose = require('mongoose');
+
+const app = express();
+const port = 3000;
+
+app.use(express.static('public'));
+app.set('view engine', 'ejs');
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
+
+mongoose.connect('mongodb://localhost:27017/userDB', { useNewUrlParser: true });
+
+const userSchema = {
+  email: String,
+  password: String,
+};
+
+const User = mongoose.model('User', userSchema);
+
+app.get('/', (req, res) => {
+  res.render('home');
+});
+
+app.get('/login', (req, res) => {
+  res.render('login');
+});
+
+app.get('/register', (req, res) => {
+  res.render('register');
+});
+
+app.post('/register', (req, res) => {
+  const newUser = new User({
+    email: req.body.username,
+    password: req.body.password,
+  });
+
+  newUser.save((err) => {
+    if (!err) {
+      res.render('secrets');
+    }
+  });
+});
+
+app.post('/login', (req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
+
+  User.findOne({ email: username }, (err, foundData) => {
+    if (err) {
+      res.send('Error loging in with bad credentials');
+    } else {
+      if (foundData.password === password) {
+        res.render('secrets');
+      } else {
+        res.send('Error loging in with bad credentials');
+      }
+    }
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Secrets started successfully at ${port}`);
+});
